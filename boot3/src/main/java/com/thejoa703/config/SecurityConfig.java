97a78766c849c4a72e19480112c0a892c37234f9
jooyeap <eyoung071212@naver.com>
2026-09-02 17:51:54 +0900

    ci/cd(3)

--- boot3/src/main/java/com/thejoa703/config/SecurityConfig.java
+++ boot3/src/main/java/com/thejoa703/config/SecurityConfig.java
@@ -93,7 +93,7 @@ public class SecurityConfig {
         configuration.setAllowedOriginPatterns(List.of(
             "http://localhost:3000",       // React 개발 서버
             "http://localhost:*",          // 로컬에서 뜨는 다른 포트들 (Flutter Web 등)
-            "https://*.yourdomain.com"     // 배포된 도메인 (필요시 추가)
+            "https://thejoa703.duckdns.org"     // 배포된 도메인 (필요시 추가)
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
